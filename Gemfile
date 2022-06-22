670b4f16f17551845dacf088d14c7da391906844
iamdavidpetric <david.petric99@gmail.com>
2022-06-22 14:43:41 +0300

    Resolve pull req issues

--- Gemfile
+++ Gemfile
@@ -3,11 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
-gem "rails", "~> 7.0.3"
+gem "bootsnap", require: false
 gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
+gem "rails", "~> 7.0.3"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-gem "bootsnap", require: false
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
    namespace :v1 do 
-    resources :users, only: [:index, :show, :update, :create, :destroy ]
-    resources :projects, only: [:index, :show, :update, :create, :destroy]
     resources :investors, only: [:index, :show, :update, :create, :destroy]
+    resources :projects, only: [:index, :show, :update, :create, :destroy]
+    resources :users, only: [:index, :show, :update, :create, :destroy ]
+
   end
 end
