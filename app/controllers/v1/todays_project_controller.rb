71524dfb5cbbe65f28e2f54019c2506c584b5504
iamdavidpetric <david.petric99@gmail.com>
2022-07-11 12:29:35 +0300

    Reneame keys

--- app/controllers/v1/todays_project_controller.rb
+++ app/controllers/v1/todays_project_controller.rb
@@ -9,5 +9,12 @@ module V1
       sorted_projects = Project.all.sort_by { |project| project.achieved_goal_percentage}
       render json: sorted_projects.last
     end
+
+    def quick_info
+      project_numbers = Project.all.count
+      investors_ammount = Investor.all.sum(:ammount)
+      investors_all = Investor.all.count
+      render json: { projects: project_numbers, total_ammount: investors_ammount, total_investors: investors_all } 
+    end
   end
 end
