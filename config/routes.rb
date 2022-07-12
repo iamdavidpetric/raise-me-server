d9adf2add15c808f88f6eeb63a2f6a48d5851407
iamdavidpetric <94174624+iamdavidpetric@users.noreply.github.com>
2022-07-12 10:02:00 +0300

    GF-20 - Endpoint to acces quick info (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
    namespace :v1 do 
     resources :investors, only: [:index, :show, :update, :create, :destroy]
     resources :projects, only: [:index, :show, :update, :create, :destroy]
-    resources :users, only: [:index, :show, :update, :create, :destroy ]
-    resources :todays_project, only: [:index, :show ]
+    resources :users, only: [:index, :show, :update, :create, :destroy]
+    resources :todays_project, only: [:index, :show] do 
+      collection do
+        get :quick_info
+      end
+    end
   end
 end
